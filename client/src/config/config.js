1849f93ebbb8f0d17a879b1f900ae7408ad2a76f
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-11-12 13:28:08 +0530

    fixed deployment

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://3d-modeling-rho.vercel.app/api/v1/dalle",
+    backendUrl: "https://3d-modeling-server.vercel.app/api/v1/dalle",
   },
 };
 
