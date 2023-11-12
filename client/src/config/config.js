6341dc167ce18576c886d6c70fdf62464cdf8d0f
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-11-12 12:58:20 +0530

    changed folder structure

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://3d-modeling-rho.vercel.app/api/v1/dalle",
+  },
+};
+
+export default config;
