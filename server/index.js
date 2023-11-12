9795c10a6df461f00e0b7a6648609e8d52c9f7ad
Divyansh Saxena <divyanshsxn998@gmail.com>
2023-11-12 12:15:55 +0530

    fixed deployment

--- server/index.js
+++ server/index.js
@@ -5,19 +5,15 @@ import cors from 'cors'
 import dalleRoutes from './routes/dalle.routes.js'
 
 dotenv.config();
-const corsOptions = {
-  origin: 'https://3d-modeling-rho.vercel.app',
-  optionsSuccessStatus: 200,
-};
 
 const app = express();
-app.use(cors(corsOptions));
-app.use(express.json({limit: "50mb"}))
+app.use(cors());
+app.use(express.json({ limit: "50mb" }))
 
 app.use('/api/v1/dalle', dalleRoutes)
 
-app.get('/',(req, res) => {
-    res.status(200).json({message: "Hello from DALLE"})
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from DALLE" })
 })
 
-app.listen(8080, ()=>console.log('Server has started at port 8080'))
+app.listen(8080, () => console.log('Server has started at port 8080'))
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -36,7 +36,7 @@ const Customizer = () => {
           readFile={readFile}
         />
       case "aipicker":
-        return <AIPicker 
+        return <AIPicker
           prompt={prompt}
           setPrompt={setPrompt}
           generatingImg={generatingImg}
@@ -48,12 +48,12 @@ const Customizer = () => {
   }
 
   const handleSubmit = async (type) => {
-    if(!prompt) return alert("Please enter a prompt");
+    if (!prompt) return alert("Please enter a prompt");
 
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://3d-modeling-rho.vercel.app/api/v1/dalle', {
+      const response = await fetch(`${config.production.backendUrl}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -79,7 +79,7 @@ const Customizer = () => {
 
     state[decalType.stateProperty] = result;
 
-    if(!activeFilterTab[decalType.filterTab]) {
+    if (!activeFilterTab[decalType.filterTab]) {
       handleActiveFilterTab(decalType.filterTab)
     }
   }
@@ -87,10 +87,10 @@ const Customizer = () => {
   const handleActiveFilterTab = (tabName) => {
     switch (tabName) {
       case "logoShirt":
-          state.isLogoTexture = !activeFilterTab[tabName];
+        state.isLogoTexture = !activeFilterTab[tabName];
         break;
       case "stylishShirt":
-          state.isFullTexture = !activeFilterTab[tabName];
+        state.isFullTexture = !activeFilterTab[tabName];
         break;
       default:
         state.isLogoTexture = true;
@@ -128,7 +128,7 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab 
+                  <Tab
                     key={tab.name}
                     tab={tab}
                     handleClick={() => setActiveEditorTab(tab.name)}
@@ -144,7 +144,7 @@ const Customizer = () => {
             className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
           >
-            <CustomButton 
+            <CustomButton
               type="filled"
               title="Go Back"
               handleClick={() => state.intro = true}
