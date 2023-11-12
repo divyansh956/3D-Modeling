abfd72f1261378be6e0ec56161ad90eabcf26715
Divyansh Saxena <96046430+divyansh956@users.noreply.github.com>
2023-11-12 10:45:09 +0530

    fixed backend url

--- server/index.js
+++ server/index.js
@@ -5,15 +5,19 @@ import cors from 'cors'
 import dalleRoutes from './routes/dalle.routes.js'
 
 dotenv.config();
+const corsOptions = {
+  origin: 'https://3d-modeling-rho.vercel.app',
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({limit: "50mb"}))
 
 app.use('/api/v1/dalle', dalleRoutes)
 
 app.get('/',(req, res) => {
-    res.status(200).json({message: "Hello from DALL.E"})
+    res.status(200).json({message: "Hello from DALLE"})
 })
 
 app.listen(8080, ()=>console.log('Server has started at port 8080'))
